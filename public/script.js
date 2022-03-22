93e789d9fbf4419770f9a433bcb6458e4fa85cd2
Maurício Júnior <68245295+LopezJunior@users.noreply.github.com>
2022-03-22 18:38:26 -0300

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('#img')
+
+function alterateImage(){
+    if(img.src === "https://www.pngmart.com/files/2/Pokeball-PNG-Image.png"){
+        img.src = "https://i.pinimg.com/originals/ce/92/26/ce9226e8f1971503507bc62910376b4b.png"
+    }else{
+        img.src = "https://www.pngmart.com/files/2/Pokeball-PNG-Image.png"
+    }
+
+    
+}
